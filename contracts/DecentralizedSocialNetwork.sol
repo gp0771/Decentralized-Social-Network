35c696e224c36afb5366ff3ebaec54429a642967
Garvit <mj1117779@gmail.com>
2025-09-30 22:44:25 +0530

    Update DecentralizedSocialNetwork.sol

--- contracts/DecentralizedSocialNetwork.sol
+++ contracts/DecentralizedSocialNetwork.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: 
+// SPDX-License-Identifier: mit
 pragma solidity ^0.8.19;
 
 contract DecentralizedSocialNetwork {
@@ -1260,6 +1260,7 @@ contract DecentralizedSocialNetwork {
         return mutualFollowers;
     }
 }
+
 
 
 
